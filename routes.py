dd618149e6af045b4daec5b190f97b4c60eaf541
rezaul1020 <39581761+rezaul1020@users.noreply.github.com>
2026-01-06 20:33:53 +0600

    Refactor product listing routes for clarity

--- routes.py
+++ routes.py
@@ -108,49 +108,42 @@ def create_products():
 #     app.logger.info("[%s] Products returned", len(results))
 #     return results, status.HTTP_200_OK
 
-# @app.route("/products", methods=["GET"])
-# def list_products():
-#     """Returns a list of Products"""
-#     app.logger.info("Request to list Products...")
-#     products = []
-#     name = request.args.get("name")
-#     if name:
-#         app.logger.info("Find by name: %s", name)
-#         products = Product.find_by_name(name)
-#     else:
-#         app.logger.info("Find all")
-#         products = Product.all()
-#     results = [product.serialize() for product in products]
-#     app.logger.info("[%s] Products returned", len(results))
-#     return results, status.HTTP_200_OK
+@app.route("/products", methods=["GET"])
+def list_productsByName():
+    """Returns a list of Products"""
+    app.logger.info("Request to list Products...")
+    products = []
+    name = request.args.get("name")
+    if name:
+        app.logger.info("Find by name: %s", name)
+        products = Product.find_by_name(name)
+    #else:
+     #   app.logger.info("Find all")
+      #  products = Product.all()
+    results = [product.serialize() for product in products]
+    app.logger.info("[%s] Products returned", len(results))
+    return results, status.HTTP_200_OK
 
 ######################################################################
 # LIST PRODUCTS
 ######################################################################
 @app.route("/products", methods=["GET"])
-def list_products():
+def list_productsByAvailability():
     """Returns a list of Products"""
     app.logger.info("Request to list Products...")
     products = []
-    name = request.args.get("name")
-    category = request.args.get("category")
+    #name = request.args.get("name")
+    #category = request.args.get("category")
     available = request.args.get("available")
-    if name:
-        app.logger.info("Find by name: %s", name)
-        products = Product.find_by_name(name)
-    elif category:
-        app.logger.info("Find by category: %s", category)
-        # create enum from string
-        category_value = getattr(Category, category.upper())
-        products = Product.find_by_category(category_value)
-    elif available:
+    
+    if available:
         app.logger.info("Find by available: %s", available)
         # create bool from string
         available_value = available.lower() in ["true", "yes", "1"]
         products = Product.find_by_availability(available_value)
     else:
-        app.logger.info("Find all")
-        products = Product.all()
+        app.logger.error("No argument found")
+        
     results = [product.serialize() for product in products]
     app.logger.info("[%s] Products returned", len(results))
     return results, status.HTTP_200_OK
